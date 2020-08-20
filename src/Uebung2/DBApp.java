a731a1839ca290413d757599cedf812c5be4e922
kbuhlert <kbuhlert@gmail.com>
2020-08-20 13:33:39 +0200

    Uebung2

--- src/Uebung2/DBApp.java
+++ src/Uebung2/DBApp.java
@@ -3,5 +3,16 @@ package Uebung2;
 public class DBApp {
     public static void main(String[] args) {
 
+        DBMethoden dbProdukt = new DBMethoden();
+
+        dbProdukt.openProductDB();
+        dbProdukt.CreateTableProdukte();
+        dbProdukt.AddProducts("Toilettenpapier", 4.5);
+        dbProdukt.AddProducts("Desinfektionsmittel", 17.5);
+        dbProdukt.AddProducts("Atemschutzmaske", 7.5);
+        dbProdukt.AddProducts("Impfstoff", 57.0);
+        dbProdukt.ReadAllProducts();
+
+
     }
 }
--- src/Uebung2/DBMethoden.java
+++ src/Uebung2/DBMethoden.java
@@ -1,8 +1,6 @@
 package Uebung2;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DBMethoden {
     private Connection con = null;
@@ -19,7 +17,77 @@ public class DBMethoden {
     }
 
     public void CreateTableProdukte(){
-        String SQLCreate = "CREATE TABLE Produkte (Nettopreis DECIMAL (5,2), Bezeichnung VARCHAR(50));";
+        String SQLCreateTable = "CREATE TABLE Produkte (Bezeichnung VARCHAR(50), Nettopreis DECIMAL(5,2));";
+
+        try {
+            Statement stm = con.createStatement();
+
+            stm.executeUpdate("DROP TABLE Produkte");
+            stm.executeUpdate(SQLCreateTable);
+            System.out.println("Tabelle wurde erstellt");
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public void AddProducts (String bezeichnung, Double preis){
+        String SQLInsert = " INSERT INTO Produkte VALUES('" + bezeichnung + "', " + preis + ")";
+
+        try {
+            Statement stm = con.createStatement();
+
+            stm.executeUpdate(SQLInsert);
+
+            System.out.println("Produkt " + bezeichnung + " wurde zur Tabelle zugefügt.");
+
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public void ReadAllProducts(){
+        System.out.println();
+        System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+        System.out.println();
+        String query = "SELECT * FROM Produkte";
+
+        try {
+            Statement stm = con.createStatement();
+            ResultSet rs = stm.executeQuery(query);
+
+            //Ausgabe der Spaltenbezeichnungen:
+            int colums = rs.getMetaData().getColumnCount();
+            for (int i=1; i<=colums; i++){
+                System.out.print(String.format("%-30s", rs.getMetaData().getColumnLabel(i)));
+            }
+            System.out.println();
+            System.out.println("-------------------------------------------------");
+
+            //Ausgabe des resultsets (=der in der Tabelle enthaltenen Daten)
+            Double gesamtpreis = 0.0;
+            int counter = 0;
+            while (rs.next()){
+                double preis = rs.getDouble("Nettopreis");
+                gesamtpreis += preis;
+                counter++;
+
+                //Ausgabe der Spalteninhalte (Werte in der Tabelle)
+                for (int i=1; i<=colums; i++){
+                    System.out.print(String.format("%-30s", rs.getString(i)));
+                }
+                System.out.println();
+            }
+
+            //Ausgabe des Durschnittspreises
+            Double averagePreis = gesamtpreis/counter;
+            System.out.println();
+            System.out.println("Durchschnittspreis der Produkte: " + averagePreis + "€");
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
 
